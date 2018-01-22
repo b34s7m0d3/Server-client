b99656caa0e2f85afa8bd33bf69914036b6a8859
Unknown <peter@petermehlmann.com>
2018-01-22 21:36:55 +0100

    Initial commit
    Initial client and server setup.

---
+++ Network_Easy/Network_Easy/ClientNetwork.cpp
@@ -0,0 +1,78 @@
+#include "ClientNetwork.h"
+
+
+
+ClientNetwork::ClientNetwork()
+{
+	// create WSADATA object
+	WSADATA wsaDATA;
+
+	// socket
+	ConnectSocket = INVALID_SOCKET;
+
+	// holds address info for socket to connect to
+	struct addrinfo *result = NULL,
+					*ptr = NULL,
+					*hints;
+
+	// initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaDATA);
+
+	if (iResult != 0) {
+		printf("WSAStartup failed with error: %d\n", iResult);
+		exit(1);
+	}
+
+	// set address info
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP; // TCP connection !!!
+
+	// resolve address and port
+	iResult = getaddrinfo("127.0.0.1", DEFAULT_PORT, &hints, &result);
+
+	if (iResult != 0) {
+		printf("getaddrinfo failed with error: %d\n", iResult);
+		WSACleanup();
+		exit(1);
+	}
+
+	// Attempt to connect to an address until one succeeds
+	for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+
+		// Create a SOCKET for connecting to server
+		ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+
+		if (ConnectSocket == INVALID_SOCKET) {
+			printf("socket failed with error: %ld\n", WSAGetLastError());
+			WSACleanup();
+			exit(1);
+		}
+
+		// Connect to server
+		iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+
+		if (iResult == SOCKET_ERROR) {
+			closesocket(ConnectSocket);
+			ConnectSocket = INVALID_SOCKET;
+			printf("The server is down... did not connect");
+		}
+	}
+
+	// no longer need address info for server
+	freeaddrinfo(result);
+
+	// if connection failed
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Unable to connect to server\n");
+		WSACleanup();
+		exit(1);
+	}
+
+}
+
+
+ClientNetwork::~ClientNetwork()
+{
+}
---
+++ Network_Easy/Network_Easy/ClientNetwork.h
@@ -0,0 +1,32 @@
+#include <winsock2.h>
+#include <Windows.h>
+#include "NetworkServices.h"
+#include <ws2tcpip.h>
+#include <stdio.h>
+
+// buffer size
+#define DEFAULT_BUFLEN 512
+// Port to connect sockets through
+#define DEFAULT_PORT "6881"
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+
+class ClientNetwork
+{
+public:
+
+	// for error checking function calls in Winsock library
+	int iResult;
+
+	// socket for client to connect to server
+	SOCKET ConnectSocket;
+
+	// ctor/dtor
+	ClientNetwork();
+	~ClientNetwork();
+};
+
